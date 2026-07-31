c871312ead6f432903c1222934d459bee7d79203
JaxonZaleski <zaleskjn@mail.uc.edu>
2026-07-30 23:55:52 -0400

    added timer framework, it needs plant diversity

--- sick ahh farming game/Views/MainPage.xaml.cs
+++ sick ahh farming game/Views/MainPage.xaml.cs
@@ -1,10 +1,37 @@
 ﻿namespace sick_ahh_farming_game;
+using System.Timers;
 
 public partial class MainPage : ContentPage
 {
+    //Duplicate and change name and time for each plant (milliseconds)
+    Timer plantName = new Timer(30000);
     public MainPage()
     {
         InitializeComponent();
+    }
+
+    //Called when plot gets planted
+    //Name subject to change
+    //Pass a variable
+    public void plotTimer(int plotId)
+    {
+        //if-else statement, checks for which timer to start based on plotId
+        //Triggers function when time ends
+        plantName.Elapsed += fullyGrownPlant;
+        plantName.AutoReset = false;
+        plantName.Start();
+    }
+
+    private void fullyGrownPlant(object? sender, ElapsedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    //When plant grows, it changes image to grown
+    //Possibly receives a passed value for the plot
+    public void fullyGrownPlant()
+    {
+
     }
 
     private async void FarmButton_Clicked(object sender, EventArgs e)
